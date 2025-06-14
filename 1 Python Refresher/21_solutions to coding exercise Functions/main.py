a4fb5827f60305d8ac392bf70cb061893cc61aa2
AlfaKazama <alfakazamaa@gmail.com>
2025-06-14 10:41:21 +0700

    Update solutions to coding exercise Functions

---
+++ 1 Python Refresher/21_solutions to coding exercise Functions/main.py
@@ -0,0 +1,13 @@
+# Complete the fucntion by making sure it returns 42
+def return_42():
+  # Complete function here
+  # pass # "pass" just means "do nothing". Make sure to delete this!
+  return 42
+
+# Create a function below, called my_functions, that takes two arguments and return the result of its two argument multiplied together
+def my_functions(x, y):
+  return x * y
+
+
+print(return_42())
+print(my_functions(10, 4))
