77ec13c85ce7d2c86b5b00b2a9e90c38926342c5
AlfaKazama <alfakazamaa@gmail.com>
2025-06-18 22:00:35 +0700

    Update 38 custom error classes

---
+++ 1 Python Refresher/38_custom_error_classes/main.py
@@ -0,0 +1,62 @@
+# class Book:
+#   def __init__(self, name: str, page_count: int):
+#     self.name = name
+#     self.page_count = page_count
+#     self.pages_read = 0
+
+#   def __reps__(self):
+#     return (
+#       f"<Book {self.name}, read {self.paage_read} pages out of {self.page_count}>"
+#     )
+  
+#   def read(self, pages:int):
+#     self.pages_read += pages
+#     print(f"You have now read {self.pages_read} pages out of {self.page_count}.")
+
+
+# python101 = Book("Python 101", 50)
+# # Kita memiliki 50 halaman, tatapi kita membaca 85 halaman, harusnya tidak bisa
+# python101.read(35)
+# python101.read(50)
+
+
+
+#################################################################################################################
+# Kita akan membuat jenis error baru dengan anama "TooManyPagesReadError" 
+# Kita akan membuat class "TooManyPagesReadError" (letaknya tidak masalah ada sebelum atau sesudah class Book)
+# class ini inherit dari class VallueError. kita perlu menggunakan inherit agar mempermudah dalam pembuatannya
+# jadi class TooManyPagesReadError adalah salinan dari class ValueError
+
+class TooManyPagesReadError(ValueError):
+  pass
+
+class Book:
+  def __init__(self, name: str, page_count: int):
+    self.name = name
+    self.page_count = page_count
+    self.pages_read = 0
+
+  def __reps__(self):
+    return (
+      f"<Book {self.name}, read {self.paage_read} pages out of {self.page_count}>"
+    )
+  
+  def read(self, pages:int):
+    if self.pages_read + pages > self.page_count:
+      raise TooManyPagesReadError(
+        f"You tried to read {self.pages_read + pages} pages, but this book only has {self.page_count} pages."
+      )
+    self.pages_read += pages
+    print(f"You have now read {self.pages_read} pages out of {self.page_count}.")
+
+
+python101 = Book("Python 101", 50)
+# python101.read(35)
+# python101.read(50)
+
+# Kita bisa menggunakan try catch
+try:
+  python101.read(35)
+  python101.read(50)
+except TooManyPagesReadError as e:
+  print(e)
