df7fb98c2e74bc69b28bb427bac49d1c4e69f2a8
AlfaKazama <alfakazamaa@gmail.com>
2026-01-01 15:36:26 +0700

    belajar ulang bbooleans

--- 1 Python Refresher/08_booleans/main.py
+++ 1 Python Refresher/08_booleans/main.py
@@ -1,20 +1,20 @@
-# ## Boolean Comparsion
-# print(5 == 5)
-# print(5 > 5)
-# print(10 != 10)
+## Boolean Comparsion
+print(5 == 5)
+print(5 > 5)
+print(10 != 10)
 
-# # Comparisons: ==, !=, >, <, >=, <=
+# Comparisons: ==, !=, >, <, >=, <=
 
 ##################################################################################
 
-friends = ["Rolf", "Bob"]
-abroad = ["Rolf", "Bob"]
+# friends = ["Rolf", "Bob"]
+# abroad = ["Rolf", "Bob"]
 
-print(friends == abroad) # Membandingkan isi kedua list apakah sama
+# print(friends == abroad) # Membandingkan isi kedua list apakah sama
 
-# Kita dapat menggunakan "is" keyword. kita akan mendapatkan hasil false, Karena ketika kita membuat sebuah list, list tersebut akan membuat area di memori, ketika kita membuat list dengan element yang sama maka akan ada area memori yang baru. "is" keyword akan memerika area memori tersebut apakah sama, karena area nya tidak sama maka hasilnya false
-print(friends is abroad)
+# # Kita dapat menggunakan "is" keyword. kita akan mendapatkan hasil false, Karena ketika kita membuat sebuah list, list tersebut akan membuat area di memori, ketika kita membuat list dengan element yang sama maka akan ada area memori yang baru. "is" keyword akan memerika area memori tersebut apakah sama, karena area nya tidak sama maka hasilnya false
+# print(friends is abroad)
 
-positif = True
-negatif = False
-print(positif, negatif)
+# positif = True
+# negatif = False
+# print(positif, negatif)
