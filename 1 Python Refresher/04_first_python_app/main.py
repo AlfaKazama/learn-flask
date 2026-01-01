c81ffc09c86bcabe2e102f38376507a409e4b6bb
AlfaKazama <alfakazamaa@gmail.com>
2026-01-01 15:20:45 +0700

    belajar ulang first python app

--- 1 Python Refresher/04_first_python_app/main.py
+++ 1 Python Refresher/04_first_python_app/main.py
@@ -1,10 +1,10 @@
-# user_age = int(input("Enter your age: "))
-# months = user_age * 12
-# print(f"Your age, {user_age}, is equal to {months} months.")
+user_age = int(input("Enter your age: "))
+months = user_age * 12
+print(f"Your age, {user_age}, is equal to {months} months.")
 
 ###############################################################33
-## Exercise
-user_age = int(input("Enter your age: "))
-second = 24 * 3600 * 30
-month = user_age * 12
-print(f"Your age, {user_age}, is equal to {month * second} seconds.")
+# ## Exercise
+# user_age = int(input("Enter your age: "))
+# second = 24 * 3600 * 30
+# month = user_age * 12
+# print(f"Your age, {user_age}, is equal to {month * second} seconds.")
