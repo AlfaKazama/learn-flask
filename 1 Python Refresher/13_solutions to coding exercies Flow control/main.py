d0387f470cf8577d913f62c337a6324ac0b40cce
AlfaKazama <alfakazamaa@gmail.com>
2026-01-01 15:52:09 +0700

    belajar ulang solution coding exercise flow control

--- 1 Python Refresher/13_solutions to coding exercies Flow control/main.py
+++ 1 Python Refresher/13_solutions to coding exercies Flow control/main.py
@@ -1,12 +1,12 @@
-# -- Part 1 --
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# # -- Part 1 --
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-evens = []
-for number in numbers:
-  if number % 2 == 0:
-    evens.append(number)
+# evens = []
+# for number in numbers:
+#   if number % 2 == 0:
+#     evens.append(number)
 
-print(evens)
+# print(evens)
 
 # -- Part 2, must be completed before submitting! --
 while True:
