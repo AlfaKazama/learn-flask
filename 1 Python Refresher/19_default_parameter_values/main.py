91d5ae56c682c4f19167e82ad5e0300204d97f5f
AlfaKazama <alfakazamaa@gmail.com>
2026-01-01 16:44:35 +0700

    belajar ulang default parameter values

--- 1 Python Refresher/19_default_parameter_values/main.py
+++ 1 Python Refresher/19_default_parameter_values/main.py
@@ -1,14 +1,14 @@
-# # Disini kita memiliki 2 parameter x dan y
-# # kita bisa menggunakan default parameter (nilai default pada parameter)
-# # Jika kita melakukan seperti dibawah ini, y adalah optional parameter, jadi tidak apa untuk tidak memberikan value y saaat memanggil function
-# # tapi ingat x tetap harus diberi value saat memanggil function, karena x tidak menggunakan default parameter
-# def add(x, y=8):
-#   print(x + y)
+# Disini kita memiliki 2 parameter x dan y
+# kita bisa menggunakan default parameter (nilai default pada parameter)
+# Jika kita melakukan seperti dibawah ini, y adalah optional parameter, jadi tidak apa untuk tidak memberikan value y saaat memanggil function
+# tapi ingat x tetap harus diberi value saat memanggil function, karena x tidak menggunakan default parameter
+def add(x, y=8):
+  print(x + y)
 
-# # add(5, 8)
-# add(5)
-# add(x=5)
-# add(7, 11)
+# add(5, 8)
+add(5)
+add(x=5)
+add(7, 11)
 
 ########################################################################################################
 
@@ -31,15 +31,15 @@
 
 ########################################################################################################
 
-# Kita tidak bisa menset parameter default dengan nilai dari variabel karena akan berakibat buruk, jadi tidak di rekomendasikan
-default_y = 3
+# # Kita tidak bisa menset parameter default dengan nilai dari variabel karena akan berakibat buruk, jadi tidak di rekomendasikan
+# default_y = 3
 
-def add(x, y=default_y):
-  sum = x + y
-  print(sum)
+# def add(x, y=default_y):
+#   sum = x + y
+#   print(sum)
 
-add(2)
+# add(2)
 
-default_y = 4
-# kita akan mendapatkan value yang sama seperti sebelumnya, karena mengubah variable yang telah digunakan tidak memodifikasi function. jadi nilai default_y  = 3 akan didefinisikan saat function dibuat
-add(2) 
+# default_y = 4
+# # kita akan mendapatkan value yang sama seperti sebelumnya, karena mengubah variable yang telah digunakan tidak memodifikasi function. jadi nilai default_y  = 3 akan didefinisikan saat function dibuat
+# add(2) 
