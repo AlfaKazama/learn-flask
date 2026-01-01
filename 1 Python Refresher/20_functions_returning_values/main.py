abb175aedbcafdfbb65c6d3ada312c715e41f179
AlfaKazama <alfakazamaa@gmail.com>
2026-01-01 16:51:57 +0700

    belajar ulang function return values

--- 1 Python Refresher/20_functions_returning_values/main.py
+++ 1 Python Refresher/20_functions_returning_values/main.py
@@ -1,20 +1,20 @@
-# ## Function returning
+## Function returning
 
-# def add(x, y):
-#   print(x + y)
+def add(x, y):
+  print(x + y)
 
-# add(5, 8)
+add(5, 8)
 
-# # jika kita melakukan ini, kita akan menjalankan function add() karena kita memanggilnya dan kemudian kita menetapkan nilainya ke variable result dan kita mencetak result variable
-# # tapi saat mencetak result kita akan mencetak "None"
-# # None adalah spesial function pada python yang berarti tidak ada value, missing value, atau undeclared value
-# # itu berarti bahwa function add(10, 2), ketika kita selesai menjalankannya, akan membuat variable result menghasilkan none
-# result = add(10, 2)
-# print(result)
+# jika kita melakukan ini, kita akan menjalankan function add() karena kita memanggilnya dan kemudian kita menetapkan nilainya ke variable result dan kita mencetak result variable
+# tapi saat mencetak result kita akan mencetak "None"
+# None adalah spesial function pada python yang berarti tidak ada value, missing value, atau undeclared value
+# itu berarti bahwa function add(10, 2), ketika kita selesai menjalankannya, akan membuat variable result menghasilkan none
+result = add(10, 2)
+print(result)
 
-# # jika kamu melakukan ini, maka akan mencetak apa yang dicetak function dan none
-# # add function() akan dijalankan terlebih dahulu, print akan mencetak x + y, dan kemudian akan mengembalikan none dan mencetak none
-# print(add(11, 9))
+# jika kamu melakukan ini, maka akan mencetak apa yang dicetak function dan none
+# add function() akan dijalankan terlebih dahulu, print akan mencetak x + y, dan kemudian akan mengembalikan none dan mencetak none
+print(add(11, 9))
 
 #################################################################################################################
 
@@ -54,20 +54,20 @@
 
 #################################################################################################################
 
-# Menggunakan function yang sebelumnya kita pelajari
+# # Menggunakan function yang sebelumnya kita pelajari
 
-def divide(dividend, divisor):
-  if divisor != 0:
-    return dividend / divisor
-  else:
-    return "You fool!"
+# def divide(dividend, divisor):
+#   if divisor != 0:
+#     return dividend / divisor
+#   else:
+#     return "You fool!"
 
-# result = divide(15, 3)
+# # result = divide(15, 3)
 
-# function diatas mereturn 2 tipe, yang pertama integer, yang kedua string. Function seperti ini tidak disarankan sebenarnya, karena misal kita melakukan perubahan operation maka akan seperti ini
-result = divide(15, 3) * 5 # ini akan berhasil karena function mereturn integer 
-print(result)
+# # function diatas mereturn 2 tipe, yang pertama integer, yang kedua string. Function seperti ini tidak disarankan sebenarnya, karena misal kita melakukan perubahan operation maka akan seperti ini
+# result = divide(15, 3) * 5 # ini akan berhasil karena function mereturn integer 
+# print(result)
 
-result = divide(15, 0) * 5 # ini akan mencetak string sbanyak 5 kali
-print(result)
+# result = divide(15, 0) * 5 # ini akan mencetak string sbanyak 5 kali
+# print(result)
 
