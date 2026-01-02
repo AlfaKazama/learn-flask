f9e47782055d4da297f9577a8297db45081ab387
AlfaKazama <alfakazamaa@gmail.com>
2026-01-02 11:14:42 +0700

    belajar ulang classmethod staticmethod

--- 1 Python Refresher/30_classmethod_staticmethod/main.py
+++ 1 Python Refresher/30_classmethod_staticmethod/main.py
@@ -1,17 +1,17 @@
-# class ClassTest:
+class ClassTest:
 
-#   # Semua function didalam class yang menggunakan object (self) sebagai parameter pertama disebut instance methods
-#   # method ini untuk mendapatkan instance
-#   def instance_method(self):
-#     print(f"called instance_method of {self}")
+  # Semua function didalam class yang menggunakan object (self) sebagai parameter pertama disebut instance methods
+  # method ini untuk mendapatkan instance
+  def instance_method(self):
+    print(f"called instance_method of {self}")
 
 
-# # Membuat object bertipe classTest (atau kita bisa menyebut proses ini: membuat instance dari classTest)
-# test = ClassTest()
+# Membuat object bertipe classTest (atau kita bisa menyebut proses ini: membuat instance dari classTest)
+test = ClassTest()
 
-# # Mendapatkan instance_method dari object yang kita buat
-# test.instance_method() # bisa gini
-# ClassTest.instance_method(test) # bisa juga gini
+# Mendapatkan instance_method dari object yang kita buat
+test.instance_method() # bisa gini
+ClassTest.instance_method(test) # bisa juga gini
 
 
 #################################################################################################################
@@ -35,6 +35,7 @@
 
 # # kita tidak memerlukan instance dalam memanggil @classmethod
 # ClassTest.class_method() ## menuliskan seperti ini, sama saja dengan seperti ini: ClassTest.class_method(ClassTest)
+# ClassTest().class_method()
 
 #################################################################################################################
 
@@ -66,36 +67,36 @@
 
 #################################################################################################################
 
-class Book:
-  # Membuat properties
-  TYPES = ("hardcover", "paperback")
+# class Book:
+#   # Membuat properties
+#   TYPES = ("hardcover", "paperback")
 
-  def __init__(self, name, book_type, weight):
-    self.name = name
-    self.book_type = book_type
-    self.weight = weight
+#   def __init__(self, name, book_type, weight):
+#     self.name = name
+#     self.book_type = book_type
+#     self.weight = weight
   
-  def __repr__(self):
-    return f"<Book {self.name}, {self.book_type}, weighing {self.weight}g>"
+#   def __repr__(self):
+#     return f"<Book {self.name}, {self.book_type}, weighing {self.weight}g>"
   
-  @classmethod
-  def hardcover(cls, name, page_weight):
-    # return Book(name, cls.TYPES[0], page_weight + 100)
-    return cls(name, cls.TYPES[0], page_weight + 100)
+#   @classmethod
+#   def hardcover(cls, name, page_weight):
+#     # return Book(name, cls.TYPES[0], page_weight + 100)
+#     return cls(name, cls.TYPES[0], page_weight + 100)
   
-  @classmethod
-  def paperback(cls, name, page_weight):
-    return cls(name, cls.TYPES[1], page_weight)
+#   @classmethod
+#   def paperback(cls, name, page_weight):
+#     return cls(name, cls.TYPES[1], page_weight)
 
-# # Mengakses properties
-# print(Book.TYPES)
+# # # Mengakses properties
+# # print(Book.TYPES)
 
-# book = Book("Harry Potter", "comic book", 1500)
-# # print(book.name)
+# # book = Book("Harry Potter", "comic book", 1500)
+# # # print(book.name)
+# # print(book)
+
+# book = Book.hardcover("Hary Potter", 1500)
+# light = Book.paperback("Python 101", 600)
+
 # print(book)
-
-book = Book.hardcover("Hary Potter", 1500)
-light = Book.paperback("Python 101", 600)
-
-print(book)
-print(light)
+# print(light)
