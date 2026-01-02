d9ef6910c81aa10a0de830fc29730c2c4394362c
AlfaKazama <alfakazamaa@gmail.com>
2026-01-02 10:19:44 +0700

    belajar ulang solution exercise classes and object

--- 1 Python Refresher/29_solutions to coding exercise Classes and objects/main.py
+++ 1 Python Refresher/29_solutions to coding exercise Classes and objects/main.py
@@ -1,38 +1,3 @@
-# class Store:
-#   def __init__(self, name):
-#     # You'll need 'name' as an argument to this method
-#     # Then, initialise 'self.name' to be the argument, and 'self.items' to be an empty list.
-#     self.name = name
-#     self.items = []
-
-#   def add_items(self, name, price):     
-#     # Create a dictionary with keys name and price, and append that to self.items.
-#     self.items.append({name: price})
-
-#   def stock_price(self):
-#     # Add together all item prices in self.items and return the total.
-#     total = 0
-    
-#     # for price in self.items:
-#     #   total += list(price.values())[0]
-#     #   print(total)
-
-#     for item in self.items:
-#       for price in item.values():
-#         total += price
-#     return total
-  
-
-# jurmiStore = Store("Jurmi Store")
-# jurmiStore.add_items("14 watt hannoc lamp", 27000)
-# jurmiStore.add_items("4-hole terminal", 19000)
-# jurmiStore.add_items("100 meters of 2.5 fiber power cable", 235000)
-# print(jurmiStore.items)
-# print(jurmiStore.stock_price())
-
-#################################################################################################################
-## Solution from videos
-
 class Store:
   def __init__(self, name):
     # You'll need 'name' as an argument to this method
@@ -42,18 +7,20 @@ class Store:
 
   def add_items(self, name, price):     
     # Create a dictionary with keys name and price, and append that to self.items.
-    item = {"name": name, "price": price}
-    self.items.append(item)
+    self.items.append({name: price})
 
   def stock_price(self):
-    ## Add together all item prices in self.items and return the total.
-    # total = 0
-    # for item in self.items:
-    #   total += item["price"]
-    # return total
+    # Add together all item prices in self.items and return the total.
+    total = 0
+    
+    # for price in self.items:
+    #   total += list(price.values())[0]
+    #   print(total)
 
-    # Kita bisa menggunakan list comprehension
-    return sum([item["price"] for item in self.items])
+    for item in self.items:
+      for price in item.values():
+        total += price
+    return total
   
 
 jurmiStore = Store("Jurmi Store")
@@ -62,3 +29,36 @@ jurmiStore.add_items("4-hole terminal", 19000)
 jurmiStore.add_items("100 meters of 2.5 fiber power cable", 235000)
 print(jurmiStore.items)
 print(jurmiStore.stock_price())
+
+#################################################################################################################
+# ## Solution from videos
+
+# class Store:
+#   def __init__(self, name):
+#     # You'll need 'name' as an argument to this method
+#     # Then, initialise 'self.name' to be the argument, and 'self.items' to be an empty list.
+#     self.name = name
+#     self.items = []
+
+#   def add_items(self, name, price):     
+#     # Create a dictionary with keys name and price, and append that to self.items.
+#     item = {"name": name, "price": price}
+#     self.items.append(item)
+
+#   def stock_price(self):
+#     ## Add together all item prices in self.items and return the total.
+#     # total = 0
+#     # for item in self.items:
+#     #   total += item["price"]
+#     # return total
+
+#     # Kita bisa menggunakan list comprehension
+#     return sum([item["price"] for item in self.items])
+  
+
+# jurmiStore = Store("Jurmi Store")
+# jurmiStore.add_items("14 watt hannoc lamp", 27000)
+# jurmiStore.add_items("4-hole terminal", 19000)
+# jurmiStore.add_items("100 meters of 2.5 fiber power cable", 235000)
+# print(jurmiStore.items)
+# print(jurmiStore.stock_price())
