b9a9b4e9b25ff00b34df947e9a3baf208e679d6f
AlfaKazama <alfakazamaa@gmail.com>
2026-01-02 10:02:31 +0700

    belajar ulang object oriented programming in python

--- 1 Python Refresher/27_object_oriented_programming_in_python/main.py
+++ 1 Python Refresher/27_object_oriented_programming_in_python/main.py
@@ -1,13 +1,13 @@
-# student = {"name": "Rolf", "grades": (89, 90, 93, 78, 90)}
+student = {"name": "Rolf", "grades": (89, 90, 93, 78, 90)}
 
-# def average(sequence):
-#   return sum(sequence) / len(sequence)
+def average(sequence):
+  return sum(sequence) / len(sequence)
 
-# print(average(student["grades"]))
+print(average(student["grades"]))
 
-# # Kita ingin memanggil method dengan cara seperti dibawah ini. Ini bisa dilakukan pada OOP, tapi untuk melakukan ini, "student" tidak bisa menjadi sebuah dictionary, karena pada dictionary kita tidak bisa melakukan ini
-# # kita harus membaut kode kita sendiri di python yang memungkinkan kita memangggila average method didalamnya
-# # print(student.average()) # ini akan error, karena kita blum bisa melakukannya
+# Kita ingin memanggil method dengan cara seperti dibawah ini. Ini bisa dilakukan pada OOP, tapi untuk melakukan ini, "student" tidak bisa menjadi sebuah dictionary, karena pada dictionary kita tidak bisa melakukan ini
+# kita harus membaut kode kita sendiri di python yang memungkinkan kita memangggila average method didalamnya
+# print(student.average()) # ini akan error, karena kita blum bisa melakukannya
 
 #################################################################################################################
 
@@ -30,30 +30,34 @@
 # print(student.grades)
 
 # # print(Student.average_grade(student))
+# # print(Student.average_grade(Student()))
 # print(student.average_grade())
+
+# print(Student())
+# print(Student)
 
 #################################################################################################################
 
-# Membuar class dengan constructor yang memiliki parameter
+# # Membuat class dengan constructor yang memiliki parameter
 
-class Student:
-  # Ini disebut constructor
-  def __init__(self, name, grades):
-    self.name = name # ini properties
-    self.grades = grades
+# class Student:
+#   # Ini disebut constructor
+#   def __init__(self, name, grades):
+#     self.name = name # ini properties
+#     self.grades = grades
 
-  # Ini disebut method
-  def average_grade(self):
-    return sum(self.grades) / len(self.grades)
+#   # Ini disebut method
+#   def average_grade(self):
+#     return sum(self.grades) / len(self.grades)
 
 
-# Membuat 2 object student dari class Student
-student1 = Student(name="Bob", grades=(100, 100, 93, 78, 90))
-student2 = Student("Rolf", (90, 90, 93, 78, 90))
+# # Membuat 2 object student dari class Student
+# student1 = Student(name="Bob", grades=(100, 100, 93, 78, 90))
+# student2 = Student("Rolf", (90, 90, 93, 78, 90))
 
-print(student1, student2) # object disimpan pada alamat memori yang berbeda
-print(student1.name)
-print(student2.name)
+# print(student1, student2) # object disimpan pada alamat memori yang berbeda
+# print(student1.name)
+# print(student2.name)
 
-print(student1.average_grade())
-print(student2.average_grade())
+# print(student1.average_grade())
+# print(student2.average_grade())
