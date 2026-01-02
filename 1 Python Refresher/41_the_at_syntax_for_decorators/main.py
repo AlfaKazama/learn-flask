9ee56cf9a5af380589627e132fa02014190de40d
AlfaKazama <alfakazamaa@gmail.com>
2026-01-02 15:34:00 +0700

    belajar ulang the at syntax fir decorator

--- 1 Python Refresher/41_the_at_syntax_for_decorators/main.py
+++ 1 Python Refresher/41_the_at_syntax_for_decorators/main.py
@@ -1,22 +1,22 @@
-# user = {"username": "jose", "access_level": "guest"}
+user = {"username": "jose", "access_level": "guest"}
 
-# def get_admin_password():
-#   return "1234"
+def get_admin_password():
+  return "1234"
 
-# def make_secure(func):
-#   def secure_function():
-#     if user["access_level"] == "admin":
-#       return func()
-#     else:
-#       return f"No admin permission for {user["username"]}"
+def make_secure(func):
+  def secure_function():
+    if user["access_level"] == "admin":
+      return func()
+    else:
+      return f"No admin permission for {user["username"]}"
   
-#   return secure_function
+  return secure_function
 
-# # Dari pada melakukan ini, yang dapat menyebabkan error jika kita memanggil get_admin_password() sebelum line code ini kita dapat menggunakan syntax @
-# # @ akan melakukan hal yang sama persis
-# get_admin_password = make_secure(get_admin_password)
+# Dari pada melakukan ini, yang dapat menyebabkan error jika kita memanggil get_admin_password() sebelum line code ini kita dapat menggunakan syntax @
+# @ akan melakukan hal yang sama persis
+get_admin_password = make_secure(get_admin_password)
 
-# print(get_admin_password())
+print(get_admin_password())
 
 #################################################################################################################
 # # Menerapkan @, ini akan sama persis cara kerjanya tanpa kita harus mendefinisikan get_admin_password = make_secure(get_admin_password)
@@ -49,29 +49,29 @@
 
 #################################################################################################################
 
-# Untuk menghindari hal diatas, kita dapat menggunakan decorator lain, kita akan mengimport functools, modul bawaan python
-# kemudian kita akan decorate secure function kita 
+# # Untuk menghindari hal diatas, kita dapat menggunakan decorator lain, kita akan mengimport functools, modul bawaan python
+# # kemudian kita akan decorate secure function kita 
 
-import functools
+# import functools
 
-user = {"username": "jose", "access_level": "guest"}
+# user = {"username": "jose", "access_level": "guest"}
 
 
-def make_secure(func):
-  # kita akan memberitahu secure_function bawha dia adalah wrapper(pembungkus) dari func
-  # apa yang kita lakukan adalah menyimpan name dan documentation dari function get_admin_password() (jika ada)
-  @functools.wraps(func)
-  def secure_function():
-    if user["access_level"] == "admin":
-      return func()
-    else:
-      return f"No admin permission for {user["username"]}"
+# def make_secure(func):
+#   # kita akan memberitahu secure_function bawha dia adalah wrapper(pembungkus) dari func
+#   # apa yang kita lakukan adalah menyimpan name dan documentation dari function get_admin_password() (jika ada)
+#   @functools.wraps(func)
+#   def secure_function():
+#     if user["access_level"] == "admin":
+#       return func()
+#     else:
+#       return f"No admin permission for {user["username"]}"
   
-  return secure_function
+#   return secure_function
 
-@make_secure
-def get_admin_password():
-  return "1234"
+# @make_secure
+# def get_admin_password():
+#   return "1234"
 
-print(get_admin_password.__name__)
+# print(get_admin_password.__name__)
 
