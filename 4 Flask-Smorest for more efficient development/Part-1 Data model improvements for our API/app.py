4a5e5395c1eba3ec27742eadad1f11e6f5cc514b
AlfaKazama <alfakazamaa@gmail.com>
2026-01-05 15:42:30 +0700

    belajar ulang data model improvements

--- 4 Flask-Smorest for more efficient development/Part-1 Data model improvements for our API/app.py
+++ 4 Flask-Smorest for more efficient development/Part-1 Data model improvements for our API/app.py
@@ -17,6 +17,9 @@ app = Flask(__name__)
 #   },
 # ]
 
+
+###############################################################################
+
 @app.get("/store") # http://127.0.0.1:5000/store
 def get_stores():
   return {"stores": list(stores.values())}
