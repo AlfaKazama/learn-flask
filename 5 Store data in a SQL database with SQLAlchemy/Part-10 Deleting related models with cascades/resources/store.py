81989512fd9b5282ccda4c3a3675583c65d3ce7b
AlfaKazama <alfakazamaa@gmail.com>
2026-01-15 17:15:29 +0700

    Part 10 deleting related models with cascades

---
+++ 5 Store data in a SQL database with SQLAlchemy/Part-10 Deleting related models with cascades/resources/store.py
@@ -0,0 +1,57 @@
+import uuid
+# from flask import request
+from flask.views import MethodView
+from flask_smorest import abort, Blueprint
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+
+from db import db
+from models import StoreModel
+from schemas import StoreSchema
+
+
+blp = Blueprint("stores", __name__, description="Operations on stores")
+
+# Kita juga akan membuat decorator response seperti pada item.py
+# kita tidak perlu decorate pada delete, karna delete hanya mengembalikan pesan saja.
+
+@blp.route("/store/<string:store_id>")
+class Store(MethodView):
+  @blp.response(200, StoreSchema)
+  def get(self, store_id):
+    # kita melakukan hal yang mirip seperti pada itemm
+    store = StoreModel.query.get_or_404(store_id)
+    return store
+
+  ## Mendelete model dari database
+  def delete(self, store_id):
+    store = StoreModel.query.get_or_404(store_id)
+    db.session.delete(store)
+    db.session.commit()
+    return {"message": "Store deleted."}
+
+@blp.route("/store")
+class StoreList(MethodView):
+
+  ## kita melakukan hal yang sama seperti pada item
+  @blp.response(200, StoreSchema(many=True))
+  def get(self):
+    return StoreModel.query.all()
+
+  # Menambahkan schema
+  @blp.arguments(StoreSchema)
+  @blp.response(201, StoreSchema)
+  def post(self, store_data):
+    store = StoreModel(**store_data)
+    try:
+      db.session.add(store)
+      db.session.commit()
+    except IntegrityError:
+      abort(
+        400,
+        message="A store with that name already exists."
+      )
+    except SQLAlchemyError:
+      abort(500, message="An error ouccurred creating store")
+    
+    return store
+
